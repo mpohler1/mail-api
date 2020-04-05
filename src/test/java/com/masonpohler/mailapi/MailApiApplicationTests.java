591529ba8f0288fe873958a5cbac012eee986553
mpohler1 <masonpohler@gmail.com>
2020-04-04 23:54:41 -0500

    Initialized project with Spring Initialzr

---
+++ src/test/java/com/masonpohler/mailapi/MailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masonpohler.mailapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
