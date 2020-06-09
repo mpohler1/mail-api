269d38cd1042f30ec82a7820814fb8db12b14fc7
mpohler1 <masonpohler@gmail.com>
2020-06-09 18:00:11 -0500

    changed naming of env variables and changed message contents of messageToDestination

--- src/main/java/com/masonpohler/api/mail/MailController.java
+++ src/main/java/com/masonpohler/api/mail/MailController.java
@@ -1,27 +1,30 @@
 package com.masonpohler.api.mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 class MailController {
     private static final String DESTINATION_EMAIL_ADDRESS = System.getenv("DESTINATION_EMAIL_ADDRESS");
 
-    @Autowired
     private JavaMailSender mailSender;
+
+    @Autowired
+    MailController(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
     @CrossOrigin("*")
     @PostMapping("/mail/send")
+    @ResponseStatus(value = HttpStatus.OK)
     void sendMail(@RequestBody Mail mail) {
         SimpleMailMessage messageToDestination = new SimpleMailMessage();
         messageToDestination.setTo(DESTINATION_EMAIL_ADDRESS);
         messageToDestination.setSubject(mail.getSubject());
-        messageToDestination.setText("The message below is from: " + mail.getFrom() + ":\n" + mail.getBody());
+        messageToDestination.setText("The message below is from " + mail.getFrom() + "\n" + mail.getBody());
         mailSender.send(messageToDestination);
 
         SimpleMailMessage messageToUser = new SimpleMailMessage();
