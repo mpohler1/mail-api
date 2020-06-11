3e758c4ff1b7f9edd12f3363b0e063c2e824824f
mpohler1 <masonpohler@gmail.com>
2020-06-11 16:40:50 -0500

    Added SendGridMailStringFactory and wrote unit tests for SendGridMailService

--- src/main/java/com/masonpohler/api/service/SendGridMailService.java
+++ src/main/java/com/masonpohler/api/service/SendGridMailService.java
@@ -1,11 +1,9 @@
 package com.masonpohler.api.service;
 
+import com.masonpohler.api.service.util.SendGridMailStringFactory;
 import com.sendgrid.Method;
 import com.sendgrid.Request;
 import com.sendgrid.SendGrid;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,39 +12,38 @@ import java.io.IOException;
 @Component
 public class SendGridMailService implements MailService {
 
-    private static final Email FROM_EMAIL = new Email(System.getenv("MAIL_USERNAME"));
     private static final String MAIL_ENDPOINT = "mail/send";
 
     private SendGrid sendGrid;
+    private SendGridMailStringFactory mailStringFactory;
 
     @Autowired
-    SendGridMailService(SendGrid sendGrid) {
+    SendGridMailService(SendGrid sendGrid, SendGridMailStringFactory mailStringFactory) {
         this.sendGrid = sendGrid;
+        this.mailStringFactory = mailStringFactory;
     }
 
     @Override
     public void sendMail(String to, String subject, String body) throws MailServiceException {
-        Mail mail = buildMail(to, subject, body);
-        Request request = buildRequest(mail);
+        String mailString = createMailString(to, subject, body);
+        Request request = createRequest(mailString);
         sendRequest(request);
     }
 
-    private Mail buildMail(String to, String subject, String body) {
-        Email toEmail = new Email(to);
-        Content content = new Content("text/plain", body);
-        return new Mail(FROM_EMAIL, subject, toEmail, content);
-    }
-
-    private Request buildRequest(Mail mail) throws MailServiceParseException {
+    private String createMailString(String to, String subject, String body) throws MailServiceParseException {
         try {
-            Request request = new Request();
-            request.setMethod(Method.POST);
-            request.setEndpoint(MAIL_ENDPOINT);
-            request.setBody(mail.build());
-            return request;
+            return mailStringFactory.createMailString(to, subject, body);
         } catch (IOException e) {
             throw new MailServiceParseException(e.getMessage());
         }
+    }
+
+    private Request createRequest(String mailString) {
+        Request request = new Request();
+        request.setMethod(Method.POST);
+        request.setEndpoint(MAIL_ENDPOINT);
+        request.setBody(mailString);
+        return request;
     }
 
     private void sendRequest(Request request) throws MailServiceSendException {
--- src/test/java/com/masonpohler/api/service/SendGridMailServiceTest.java
+++ src/test/java/com/masonpohler/api/service/SendGridMailServiceTest.java
@@ -1,4 +1,62 @@
 package com.masonpohler.api.service;
 
-public class SendGridMailServiceTest {
+import com.masonpohler.api.service.util.SendGridMailStringFactory;
+import com.sendgrid.Request;
+import com.sendgrid.SendGrid;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+
+class SendGridMailServiceTest {
+
+    @Mock
+    private SendGrid mockedSendGrid;
+
+    @Mock
+    private SendGridMailStringFactory mockedMailStringFactory;
+
+    @InjectMocks
+    private SendGridMailService service;
+
+    @BeforeEach
+    void set_up() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @SneakyThrows
+    @Test
+    void send_mail_throws_mail_service_parse_exception_when_create_mail_string_throws_io_exception() {
+        doThrow(IOException.class)
+                .when(mockedMailStringFactory).createMailString(
+                        any(String.class),
+                        any(String.class),
+                        any(String.class)
+        );
+
+        assertThrows(MailServiceParseException.class, () -> service.sendMail("", "", ""));
+    }
+
+    @SneakyThrows
+    @Test
+    void send_mail_throws_mail_service_send_exception_when_send_request_throws_io_exception() {
+        doThrow(IOException.class)
+                .when(mockedSendGrid).api(any(Request.class));
+
+        assertThrows(MailServiceSendException.class, () -> service.sendMail("", "", ""));
+    }
+
+    @Test
+    void send_mail_does_not_throw_exception_when_mail_string_factory_and_send_grid_do_not_throw_exceptions() {
+        assertDoesNotThrow(() -> service.sendMail("", "", ""));
+    }
 }
